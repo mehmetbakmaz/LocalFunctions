ce9b2674b31b273fcb9dc23ce67187e98dec03cb
Coders Club <mehmetbakmaz6@gmail.com>
2023-02-13 17:44:36 +0300

    Add project files.

---
+++ LocalFunctions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            localFuncKullanimi(12, 23);
+        }
+        static void localFuncKullanimi(int sayi1, int sayi2)
+        {
+            int topla(int gelensayi1, int gelensayi2)
+            {
+                return gelensayi1 + gelensayi2;
+            }
+            int toplamdeger = topla(sayi1, sayi2);
+            Console.WriteLine(toplamdeger);
+        }
+    }
+}
